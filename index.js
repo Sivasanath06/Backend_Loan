995c960aaf14dd68de18230a0261ebc09c8b171f
Sivasanath06 <100675167+Sivasanath06@users.noreply.github.com>
2022-12-29 17:36:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ import cors from "cors";
 import personRouter from "./routing/person-routes.js";
 import loanRouter from "./routing/loan-routes.js";
 import businessRouter from "./routing/business-routes.js";
-import path from 'path'
 const app = express();
 dotenv.config();
 app.use(cors());
@@ -13,10 +12,7 @@ app.use(express.json());
 app.use("/person", personRouter);
 app.use("/loan", loanRouter);
 app.use("/business", businessRouter);
-app.use(express.static(path.join(__dirname,'./client/build')))
-app.get('*',function(req,res){
-  res.sendFile(path.join(__dirname,'./client/build/index.html'));
-})
+
 mongoose
   .connect(
     `mongodb+srv://Sivasanath:${process.env.MONGODB_PASSWORD}@cluster0.c7bpxin.mongodb.net/?retryWrites=true&w=majority`
